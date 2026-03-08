d55429826e6715a488a57a3adfdbdc5828b337e2
VisualsbyShiv <visualsbyshiv@VisualsbyShivs-MacBook-Air.local>
2026-03-08 21:07:40 +0530

    Fixing delete feature

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -4,4 +4,13 @@ const { createTask, getTask, updateTask } = require('../controller/taskControlle
 routes.post('/', createTask);
 routes.get('/', getTask);
 routes.patch('/:id',updateTask);
+
+routes.delete('/:id',async (req,res)=>{
+try{
+    await task.findByIdAndDelete(req.params.id);
+    res.status(200).json({message:'succesfully Deleted'});
+}catch(error){
+    res.status(500).json(error);
+}
+})
 module.exports = routes;
