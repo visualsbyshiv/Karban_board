0644736942a1cc4860d4d5a2aad075a78733e5ea
VisualsbyShiv <visualsbyshiv@VisualsbyShivs-MacBook-Air.local>
2026-03-13 15:50:13 +0530

    Fix: defind allowedOrigins and update CORS

--- backend/server.js
+++ backend/server.js
@@ -1,64 +1,68 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const http = require('http');
-const { Server } = require('socket.io');
-const connectDB = require('./config/db')
-const taskRoutes = require('./routes/taskRoutes');
-const { error } = require('console');
+    const express = require('express');
+    const cors = require('cors');
+    const dotenv = require('dotenv');
+    const http = require('http');
+    const { Server } = require('socket.io');
+    const connectDB = require('./config/db')
+    const taskRoutes = require('./routes/taskRoutes');
+    const { error } = require('console');
 
 
-dotenv.config();
-connectDB();
+    dotenv.config();
+    connectDB();
 
-const app = express();
+    const app = express();
+const allowedOrigins = [
+    "https://karban-board.vercel.app",
+    "http://localhost:5173"
+];
 
-app.use(cors({
-   origin: function(origin,callback){
-    if(!origin|| allowedOrigins.includes(origin) || origin.endsWith(".vercel.app")){
-        callback(null,true);
-    }else{
-        callback(new error("not allowed by corsa"));
-    }
-   },
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    credentials:true
-}));
-
-
-
-const server = http.createServer(app);
-const io = new Server(server, {
-
-    cors: {
-        origin: true,
-        methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    app.use(cors({
+    origin: function(origin,callback){
+        if(!origin|| allowedOrigins.includes(origin) || origin.endsWith(".vercel.app")){
+            callback(null,true);
+        }else{
+            callback(new error("not allowed by corsa"));
+        }
+    },
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
         credentials:true
-    }
-});
-app.use(express.json());
-app.set('socketio', io);
-app.use('/api/tasks', taskRoutes);
+    }));
 
-io.on('connection', (socket) => {
-    console.log(`USER CONNECTED ${socket.id}`);
-    socket.on("task moved", (data) => {
-        socket.broadcast.emit('Board Update', data);
+
+
+    const server = http.createServer(app);
+    const io = new Server(server, {
+
+        cors: {
+            origin: true,
+            methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+            credentials:true
+        }
+    });
+    app.use(express.json());
+    app.set('socketio', io);
+    app.use('/api/tasks', taskRoutes);
+
+    io.on('connection', (socket) => {
+        console.log(`USER CONNECTED ${socket.id}`);
+        socket.on("task moved", (data) => {
+            socket.broadcast.emit('Board Update', data);
+        });
+
+        socket.on('Disconnected', () => {
+            console.log('User Discomnected');
+        });
     });
 
-    socket.on('Disconnected', () => {
-        console.log('User Discomnected');
+    app.get('/', (req, res) => {
+        res.send('server connect to db');
     });
-});
 
-app.get('/', (req, res) => {
-    res.send('server connect to db');
-});
+    const PORT = process.env.PORT || 8080;
 
-const PORT = process.env.PORT || 8080;
-
-server.listen(PORT,'0.0.0.0', () => {
-    console.log(`Server run on ${PORT}`);
-});
+    server.listen(PORT,'0.0.0.0', () => {
+        console.log(`Server run on ${PORT}`);
+    });
 
 
