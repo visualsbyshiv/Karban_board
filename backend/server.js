309b921f88c77008bce45d0d0e7c37bae63251de
VisualsbyShiv <visualsbyshiv@VisualsbyShivs-MacBook-Air.local>
2026-03-17 23:58:21 +0530

    Fix: CORS

--- backend/server.js
+++ backend/server.js
@@ -1,76 +1,84 @@
-    const express = require('express');
-    const cors = require('cors');
-    const dotenv = require('dotenv');
-    const http = require('http');
-    const { Server } = require('socket.io');
-    const connectDB = require('./config/db')
-    const taskRoutes = require('./routes/taskRoutes');
-    const authRoutes =require('./routes/auth')
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const http = require('http');
+const { Server } = require('socket.io');
+const connectDB = require('./config/db')
+const taskRoutes = require('./routes/taskRoutes');
+const authRoutes = require('./routes/auth');
+const { callbackify } = require('util');
 
 
-    dotenv.config();
-    connectDB();
+dotenv.config();
+connectDB();
 
-    const app = express();
+const app = express();
 const allowedOrigins = [
     "https://karban-board.vercel.app",
     "http://localhost:5173"
 ];
 
-    app.use(cors({
-    origin: function(origin,callback){
-        if(!origin|| allowedOrigins.includes(origin) || origin.endsWith(".vercel.app")){
-            callback(null,true);
-        }else{
+app.use(cors({
+    
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin) || origin.endsWith(".vercel.app")) {
+            callback(null, true);
+        } else {
             callback(new Error("not allowed by corsa"));
         }
     },
 
-        methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-        credentials:true
-    }));
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true
+}));
+
+app.use(express.json());
 
 
 
-    const server = http.createServer(app);
-    const io = new Server(server, {
+const server = http.createServer(app);
+const io = new Server(server, {
 
-        cors: {
-            origin: "https://karban-board.vercel.app",
-            methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
-            credentials:true
-        },
-        transports: ['websocket', 'polling'],
-        allowEIO3: true
-    });
-    app.use(express.json());
-    app.set('socketio', io);
-    app.use('/api/auth', authRoutes);
-    app.use('/api/tasks', taskRoutes);
-    
-    app.get('/api/test', (req, res) => {
-    res.json({ message: " Server is live", });
+    cors: {
+      origin:(origin,callback)=>{
+        callback(null, true);
+      },
+      methods:["GET","PODT"],
+      credentials:true
+    },
+    transports: ['websocket', 'polling'],
+    allowEIO3: true
 });
 
-    io.on('connection', (socket) => {
-        console.log(`USER CONNECTED ${socket.id}`);
-        socket.on("task moved", (data) => {
-            socket.broadcast.emit('Board Update', data);
-        });
 
-        socket.on('disconnect', () => {
-            console.log('User Disconnected');
-        });
+app.set('socketio', io);
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
+
+app.get('/api/test', (req, res) => {
+    res.json({ message: " Server is live", });
+});
+app.get('/', (req, res) => {
+    res.send('server connect to db');
+});
+io.on('connection', (socket) => {
+    console.log(`USER CONNECTED ${socket.id}`);
+    socket.on("task moved", (data) => {
+        socket.broadcast.emit('Board Update', data);
     });
 
-    app.get('/', (req, res) => {
-        res.send('server connect to db');
+    socket.on('disconnect', () => {
+        console.log('User Disconnected');
     });
+});
 
-    const PORT = process.env.PORT || 8080;
 
-    server.listen(PORT,'0.0.0.0', () => {
-        console.log(`Server run on ${PORT}`);
-    });
+
+
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server run on ${PORT}`);
+});
 
 
